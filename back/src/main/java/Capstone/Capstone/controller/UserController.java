0f6614b3b97718c0b6ddd3493cf45ffa6f5e366a
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-07-03 23:27:27 +0900

    feat : 로그 로직

--- back/src/main/java/Capstone/Capstone/controller/UserController.java
+++ back/src/main/java/Capstone/Capstone/controller/UserController.java
@@ -3,6 +3,7 @@ package Capstone.Capstone.controller;
 import Capstone.Capstone.dto.UserDTO;
 import Capstone.Capstone.service.UserService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@Slf4j
 @RequestMapping("/api")
 public class UserController {
     private final UserService userService;
@@ -20,12 +22,14 @@ public class UserController {
 
     @PostMapping("/register")
     public ResponseEntity<UserDTO> UserRegister(@Valid @RequestBody UserDTO userDTO){
+        log.info("register");
         UserDTO user = userService.RegisterUser(userDTO);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
     public ResponseEntity<UserDTO> UserLogin(@Valid @RequestBody UserDTO userDTO){
+        log.info("login");
         UserDTO user = userService.UserLogin(userDTO);
         return ResponseEntity.ok(user);
     }
---
+++ back/src/main/java/Capstone/Capstone/utils/config/WebConfig.java
@@ -0,0 +1,17 @@
+package Capstone.Capstone.utils.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*") // 필요한 경우 프론트엔드의 정확한 주소를 지정
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+
+}
