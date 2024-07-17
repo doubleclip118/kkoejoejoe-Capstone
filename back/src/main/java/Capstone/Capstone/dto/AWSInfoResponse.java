1829a42cab364f17e28d88866e7fa07d86806d6e
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-07-17 20:54:54 +0900

    feat : cloud info response add

---
+++ back/src/main/java/Capstone/Capstone/dto/AWSInfoResponse.java
@@ -0,0 +1,42 @@
+package Capstone.Capstone.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AWSInfoResponse {
+    @NotEmpty
+    private Long id;
+    @NotEmpty
+    private String DriverName;
+    @NotEmpty
+    private String ProviderName;
+    @NotEmpty
+    private String DriverLibFileName;
+    @NotEmpty
+    private String CredentialName;
+    @NotEmpty
+    private String CredentialAccessKey;
+    @NotEmpty
+    private String CredentialAccessKeyVal;
+    @NotEmpty
+    private String CredentialSecretKey;
+    @NotEmpty
+    private String CredentialSecretKeyVal;
+    @NotEmpty
+    private String RegionName;
+    @NotEmpty
+    private String RegionKey;
+    @NotEmpty
+    private String RegionValue;
+    @NotEmpty
+    private String ZoneKey;
+    @NotEmpty
+    private String ZoneValue;
+}
---
+++ back/src/main/java/Capstone/Capstone/dto/AzureInfoResponse.java
@@ -0,0 +1,46 @@
+package Capstone.Capstone.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AzureInfoResponse {
+    @NotEmpty
+    private Long id;
+    @NotEmpty
+    private String DriverName;
+    @NotEmpty
+    private String ProviderName;
+    @NotEmpty
+    private String DriverLibFileName;
+    @NotEmpty
+    private String CredentialName;
+    @NotEmpty
+    private String ClientIdkey;
+    @NotEmpty
+    private String ClientIdValue;
+    @NotEmpty
+    private String ClientSecretKey;
+    @NotEmpty
+    private String ClientSecretValue;
+    @NotEmpty
+    private String TenantIdKey;
+    @NotEmpty
+    private String TenantIdValue;
+    @NotEmpty
+    private String RegionName;
+    @NotEmpty
+    private String RegionKey;
+    @NotEmpty
+    private String RigionValue;
+    @NotEmpty
+    private String ZoneKey;
+    @NotEmpty
+    private String ZoneValue;
+}
