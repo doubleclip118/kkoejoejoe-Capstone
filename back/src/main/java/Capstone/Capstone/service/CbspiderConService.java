24a97f5c50627172823deb9035d56469be92e68c
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-07-26 22:18:36 +0900

    feat : 커넥트 성공

--- back/src/main/java/Capstone/Capstone/service/CbspiderConService.java
+++ back/src/main/java/Capstone/Capstone/service/CbspiderConService.java
@@ -4,8 +4,14 @@ import Capstone.Capstone.domain.User;
 import Capstone.Capstone.repository.AWSCloudInfoRepository;
 import Capstone.Capstone.repository.AzureCloudInfoRepository;
 import Capstone.Capstone.repository.UserRepository;
-import Capstone.Capstone.service.dto.CloudDriver;
+import Capstone.Capstone.service.dto.AWSCloudDriverDTO;
+import Capstone.Capstone.service.dto.AWSConfigDTO;
+import Capstone.Capstone.service.dto.AWSCredentialDTO;
+import Capstone.Capstone.service.dto.KeyValueInfo;
+import Capstone.Capstone.service.dto.AWSRegionDTO;
 import Capstone.Capstone.utils.error.UserNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,13 +39,67 @@ public class CbspiderConService {
                 log.error("User not found for id: {}", id);
                 return new UserNotFoundException("User Not Found");
             });
-        CloudDriver cloudDriver = new CloudDriver(user.getAwsCloudInfo().getDriverName(),
+        System.out.println(user.getAwsCloudInfo().getCredentialAccessKey());
+
+        //클라우드 드라이버 등록
+        AWSCloudDriverDTO AWSCloudDriverDTO = new AWSCloudDriverDTO(user.getAwsCloudInfo().getDriverName(),
             user.getAwsCloudInfo().getProviderName(),
             user.getAwsCloudInfo().getDriverLibFileName());
-        CloudDriver cloudDriverResponse = externalApiService.postToSpiderDriver(cloudDriver);
+        externalApiService.postToSpiderDriver(
+            AWSCloudDriverDTO);
+
+        //클라우드 크리덴셜 등록
+        KeyValueInfo awsAccess = new KeyValueInfo(
+            user.getAwsCloudInfo().getCredentialAccessKey(),
+            user.getAwsCloudInfo().getCredentialAccessKeyVal()
+        );
+
+        KeyValueInfo awsSecret = new KeyValueInfo(
+            user.getAwsCloudInfo().getCredentialSecretKey(),
+            user.getAwsCloudInfo().getCredentialSecretKeyVal()
+        );
+
+        List<KeyValueInfo> keyValueInfoList = Arrays.asList(awsAccess, awsSecret);
+
+        AWSCredentialDTO awsCredentialDTO = new AWSCredentialDTO(
+            user.getAwsCloudInfo().getCredentialName(),
+            user.getAwsCloudInfo().getProviderName(),
+            keyValueInfoList
+        );
+        externalApiService.postToSpiderCredential(awsCredentialDTO);
+
+        //리전 등록
+
+        KeyValueInfo awsRegion = new KeyValueInfo(
+            user.getAwsCloudInfo().getRegionKey(),
+            user.getAwsCloudInfo().getRegionValue()
+        );
+
+        KeyValueInfo awsZone = new KeyValueInfo(
+            user.getAwsCloudInfo().getZoneKey(),
+            user.getAwsCloudInfo().getZoneValue()
+        );
+
+        List<KeyValueInfo> RegionkeyValueInfoList = Arrays.asList(awsRegion, awsZone);
+
+        AWSRegionDTO awsRegionDTO = new AWSRegionDTO(user.getAwsCloudInfo().getRegionName(),
+            user.getAwsCloudInfo().getProviderName(),
+            RegionkeyValueInfoList);
+
+        externalApiService.postToSpiderRegion(awsRegionDTO);
+
+        //커넥션 생성
+        AWSConfigDTO awsConfigDTO = new AWSConfigDTO(user.getAwsCloudInfo().getConfigName(),
+            user.getAwsCloudInfo().getProviderName(),
+            user.getAwsCloudInfo().getDriverName(), user.getAwsCloudInfo().getCredentialName(),
+            user.getAwsCloudInfo().getRegionName());
+        externalApiService.postToSpiderConfig(awsConfigDTO);
+
 
         return "생성 완료";
     }
+
+
 
 
 }
--- back/src/main/java/Capstone/Capstone/service/ExternalApiService.java
+++ back/src/main/java/Capstone/Capstone/service/ExternalApiService.java
@@ -1,6 +1,9 @@
 package Capstone.Capstone.service;
 
-import Capstone.Capstone.service.dto.CloudDriver;
+import Capstone.Capstone.service.dto.AWSCloudDriverDTO;
+import Capstone.Capstone.service.dto.AWSConfigDTO;
+import Capstone.Capstone.service.dto.AWSCredentialDTO;
+import Capstone.Capstone.service.dto.AWSRegionDTO;
 import Capstone.Capstone.utils.error.CbSpiderServerException;
 import Capstone.Capstone.utils.error.CloudInfoIncorrectException;
 import lombok.extern.slf4j.Slf4j;
@@ -17,26 +20,103 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class ExternalApiService {
     private final RestTemplate restTemplate;
-    private final String baseUrl = "http://3.34.135.215:1024";
+    private final String baseUrl = "http://54.180.246.122:1024";
 
     public ExternalApiService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    public CloudDriver postToSpiderDriver(CloudDriver cloudDriver) {
+    public AWSCloudDriverDTO postToSpiderDriver(AWSCloudDriverDTO AWSCloudDriverDTO) {
         String endpoint = "/spider/driver";
         String fullUrl = baseUrl + endpoint;
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        HttpEntity<CloudDriver> request = new HttpEntity<>(cloudDriver, headers);
+        HttpEntity<AWSCloudDriverDTO> request = new HttpEntity<>(AWSCloudDriverDTO, headers);
         try {
             log.info("cloud aws Diver 응답요청 ");
-            CloudDriver cloudDriver1 = restTemplate.postForObject(fullUrl, request,
-                CloudDriver.class);
-            System.out.println(cloudDriver1.getDriverName() + cloudDriver1.getDriverLibFileName() + cloudDriver1.getProviderName());
-            return cloudDriver1;
+            AWSCloudDriverDTO AWSCloudDriverDTO1 = restTemplate.postForObject(fullUrl, request,
+                AWSCloudDriverDTO.class);
+            System.out.println(
+                AWSCloudDriverDTO1.getDriverName() +"\n"+ AWSCloudDriverDTO1.getDriverLibFileName() +"\n" + AWSCloudDriverDTO1.getProviderName());
+            return AWSCloudDriverDTO1;
+        } catch (HttpClientErrorException e) {
+            // 4xx 클라이언트 오류
+            throw new CloudInfoIncorrectException("InfoIncorrect");
+        } catch (HttpServerErrorException e) {
+            System.out.println(e);
+            // 5xx 서버 오류
+            throw new CbSpiderServerException("cbspider not working");
+        }
+    }
+
+    public AWSCredentialDTO postToSpiderCredential(AWSCredentialDTO awsCredentialDTO) {
+        String endpoint = "/spider/credential";
+        String fullUrl = baseUrl + endpoint;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<AWSCredentialDTO> request = new HttpEntity<>(awsCredentialDTO, headers);
+        try {
+            log.info("cloud aws credential 응답요청 ");
+            AWSCredentialDTO awsCredentialDTO1 = restTemplate.postForObject(fullUrl, request,
+                AWSCredentialDTO.class);
+            System.out.println(awsCredentialDTO1.toString());
+            System.out.println(awsCredentialDTO1);
+            return awsCredentialDTO1;
+
+        } catch (HttpClientErrorException e) {
+            // 4xx 클라이언트 오류
+            throw new CloudInfoIncorrectException("InfoIncorrect");
+        } catch (HttpServerErrorException e) {
+            System.out.println(e);
+            // 5xx 서버 오류
+            throw new CbSpiderServerException("cbspider not working");
+        }
+    }
+
+    public AWSRegionDTO postToSpiderRegion(AWSRegionDTO awsRegionDTO) {
+        String endpoint = "/spider/region";
+        String fullUrl = baseUrl + endpoint;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<AWSRegionDTO> request = new HttpEntity<>(awsRegionDTO, headers);
+        try {
+            log.info("cloud aws region 응답요청 ");
+            AWSRegionDTO awsRegionDTO1 = restTemplate.postForObject(fullUrl, request,
+                AWSRegionDTO.class);
+            System.out.println(awsRegionDTO1);
+            return awsRegionDTO1;
+
+        } catch (HttpClientErrorException e) {
+            // 4xx 클라이언트 오류
+            throw new CloudInfoIncorrectException("InfoIncorrect");
+        } catch (HttpServerErrorException e) {
+            System.out.println(e);
+            // 5xx 서버 오류
+            throw new CbSpiderServerException("cbspider not working");
+        }
+    }
+
+    public AWSConfigDTO postToSpiderConfig(AWSConfigDTO awsConfigDTO) {
+        String endpoint = "/spider/connectionconfig";
+        String fullUrl = baseUrl + endpoint;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<AWSConfigDTO> request = new HttpEntity<>(awsConfigDTO, headers);
+        try {
+            log.info("cloud aws region 응답요청 ");
+            AWSConfigDTO awsConfigDTO1 = restTemplate.postForObject(fullUrl, request,
+                AWSConfigDTO.class);
+            System.out.println(awsConfigDTO1.toString());
+            return awsConfigDTO1;
+
         } catch (HttpClientErrorException e) {
             // 4xx 클라이언트 오류
             throw new CloudInfoIncorrectException("InfoIncorrect");
@@ -45,4 +125,6 @@ public class ExternalApiService {
             throw new CbSpiderServerException("cbspider not working");
         }
     }
+
+
 }
