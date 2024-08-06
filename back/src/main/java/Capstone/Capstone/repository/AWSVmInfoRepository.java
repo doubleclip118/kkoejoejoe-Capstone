162dbb7eeecefc0a94b0f66aad4e1f0c1659da24
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-08-06 16:07:47 +0900

    feat : aws vm repo 생성

---
+++ back/src/main/java/Capstone/Capstone/repository/AWSVmInfoRepository.java
@@ -0,0 +1,8 @@
+package Capstone.Capstone.repository;
+
+import Capstone.Capstone.domain.AWSVmInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AWSVmInfoRepository extends JpaRepository<AWSVmInfo,Long> {
+
+}
