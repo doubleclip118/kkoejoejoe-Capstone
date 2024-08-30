eec95da8d6ba3405428818ae04b8d89a1b3b549d
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-08-30 22:18:22 +0900

    feat : 블록체인 네트워크 jpa 생성

---
+++ back/src/main/java/Capstone/Capstone/repository/BlockChainNetworkRepository.java
@@ -0,0 +1,8 @@
+package Capstone.Capstone.repository;
+
+import Capstone.Capstone.domain.BlockChainNetwork;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlockChainNetworkRepository extends JpaRepository<Long, BlockChainNetwork> {
+
+}
